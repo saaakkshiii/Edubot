f2f33345a8f350b051295769b667bc8c7bed06d0
saaakkshiii <115159201+saaakkshiii@users.noreply.github.com>
2025-11-09 02:40:52 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAuwgyw9CF0oLuAKieElmgCFWIGmXo7WPI",
+  authDomain: "jiit-enquiry-chatbot.firebaseapp.com",
+  projectId: "jiit-enquiry-chatbot",
+  storageBucket: "jiit-enquiry-chatbot.firebasestorage.app",
+  messagingSenderId: "1045707839864",
+  appId: "1:1045707839864:web:942103a7c1a0264b53108d",
+  measurementId: "G-ZVWDM2RMKM"
+};
+export {firebaseConfig};
